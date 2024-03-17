06d091b83630a3f0ac5b5bcb5421a139ac800299
vynrichter <vynxil@hotmail.com>
2024-03-17 22:27:33 +0000

    Initial commit

---
+++ static/script.js
@@ -0,0 +1,4 @@
+document.getElementById('stock-filter').addEventListener('click', function() {
+    var arrow = document.querySelector('.custom-dropdown .arrow');
+    arrow.style.display = arrow.style.display === 'none' ? 'inline' : 'none';
+});
